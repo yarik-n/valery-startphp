2b71abe7cc3b7ef2ed78b9bd17eba267cdf852ee
Yarik <arikpisarenko72@gmail.com>
2026-02-07 01:55:49 +0300

    Массивы, страны, животные и имена друзей (поправки)

--- php/hello.php
+++ php/hello.php
@@ -6,5 +6,5 @@ $countries = ["Canada","Italy","Australia","United States"];
     echo $countries[2];
 $pets = ["dog","cat","bird"];
     echo $pets[1];
-$name_friend = ["Vanya, Arina, Katya"];
-    echo $name_friend[1];
+$name = ["Vanya, Arina, Katya"];
+    echo $name[1];
