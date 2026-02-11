56a6f98458b5459e129013a1ef36539daac9e560
Yarik <arikpisarenko72@gmail.com>
2026-02-11 20:43:27 +0300

    База данных (корректировка)

--- marlin/pdoBase/projectStart/store.php
+++ marlin/pdoBase/projectStart/store.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=valery", "valery-startphp", '');
+$pdo = new PDO("mysql:host=localhost;dbname=root", "root", '');
 
 $sql = "INSERT INTO users (username) VALUES (:username)";
 $statment = $pdo->prepare($sql);
