24bd9ca88a4d5583d2bd99c9068eaf718e52a3e5
Yarik <arikpisarenko72@gmail.com>
2026-02-11 20:34:41 +0300

    База данных, внесение, обработка и выдача результата

---
+++ marlin/pdoBase/projectStart/store.php
@@ -0,0 +1,8 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=valery", "valery", '');
+
+$sql = "INSERT INTO users (username) VALUES (:username)";
+$statment = $pdo->prepare($sql);
+$statment->execute($_POST);
+
+header("Location: index.php");
