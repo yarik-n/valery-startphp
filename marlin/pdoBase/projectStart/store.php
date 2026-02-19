12cc55827420310142bebdf683b415489b6cbd33
yarik-noyt <arikpisarenko72@gmail.com>
2026-02-19 21:25:16 +0300

    Мелкие корректировки

--- marlin/pdoBase/projectStart/store.php
+++ marlin/pdoBase/projectStart/store.php
@@ -1,7 +1,8 @@
 <?php
+
 $pdo = new PDO('mysql:host=MySQL-8.4;dbname=users', "root", '');
 $sql = "INSERT INTO users (username) VALUES (:username)";
 $statement = $pdo->prepare($sql);
-$statement->execute($_POST);
-header('Location: /index.php');
+$statement->execute();
+//header('Location: /index.php');
 ?>
