81a6722b4cff4c570cd639aa5df3182c87b9105f
yarik-noyt <arikpisarenko72@gmail.com>
2026-02-26 20:12:14 +0300

    Данные подтягиваются из БД, можно их изменить через кнопку edit в index.php

--- marlin/pdoBase/projectStart/update.php
+++ marlin/pdoBase/projectStart/update.php
@@ -1,13 +1,14 @@
 <?php
-$examplePost = [
-    'id' => 24,
-    'name' => 'Jane',
-    'surname' => 'Scott',
-    'username' => 'jane_user',
-    'email' => 'jane3@gmail.com'
-];
+//$examplePost = [
+//    'id' => 24,
+//    'name' => 'Jane',
+//    'surname' => 'Scott',
+//    'username' => 'jane_user',
+//    'email' => 'jane@gmail.com'
+//];
 $pdo = new PDO('mysql:host=MySQL-8.4;dbname=users', "root", '');
 $sql = "UPDATE users SET name=:name, surname=:surname, username=:username, email=:email WHERE id = :id";
 $statement = $pdo->prepare($sql);
-$statement->execute($examplePost);
+$statement->execute($_POST);
+header('Location:/marlin/pdoBase/projectStart/');
 ?>
