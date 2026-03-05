5fe8a28d6727e8f3c3924da5869b179f5d411f27
yarik-noyt <arikpisarenko72@gmail.com>
2026-03-05 22:58:49 +0300

    Сделал 6 задание

---
+++ marlin/20tasks/task_6.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>
+            Подготовительные задания к курсу
+        </title>
+        <meta name="description" content="Chartist.html">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+        <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+        <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+        <link rel="stylesheet" media="screen, print" href="css/statistics/chartist/chartist.css">
+        <link rel="stylesheet" media="screen, print" href="css/miscellaneous/lightgallery/lightgallery.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+        <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+    </head>
+    <body class="mod-bg-1 mod-nav-link ">
+        <main id="js-page-content" role="main" class="page-content">
+            <div class="col-md-6">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Задание
+                        </h2>
+                        <div class="panel-toolbar">
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        </div>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                           <div class="d-flex flex-wrap demo demo-h-spacing mt-3 mb-3">
+                               <?php
+                               $item = [
+                                       [
+                                               'class' => 'rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0',
+                                               'image' => 'img/demo/authors/sunny.png',
+                                               'alt' => 'Sunny A.',
+                                               'title' => 'Contact Sunny',
+                                               'name' => 'Sunny A. (UI/UX Expert)',
+                                               'work' => 'Lead Author',
+                                               'twitter' => 'https://twitter.com/@myplaneticket',
+                                               'wrap' => 'https://wrapbootstrap.com/user/myorange',
+                                               'username' => '@myplaneticket'
+                                       ],
+                                       [
+                                               'class' => 'rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0',
+                                               'image' => 'img/demo/authors/josh.png',
+                                               'alt' => 'Jos K.',
+                                               'title' => 'Contact Jos',
+                                               'name' => 'Jos K. (ASP.NET Developer)',
+                                               'work' => 'Partner &amp; Contributor',
+                                               'twitter' => 'https://twitter.com/@atlantez',
+                                               'wrap' => 'https://wrapbootstrap.com/user/Walapa',
+                                               'username' => '@atlantez'
+                                       ],
+                                       [
+                                               'class' => 'banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0',
+                                               'image' => 'img/demo/authors/jovanni.png',
+                                               'alt' => 'Roberto R. (Rails Developer)',
+                                               'title' => 'Contact Jovanni',
+                                               'name' => 'Jovanni L. (PHP Developer)',
+                                               'work' => 'Partner &amp; Contributor',
+                                               'twitter' => 'https://wrapbootstrap.com/user/@lodev09',
+                                               'wrap' => 'https://wrapbootstrap.com/user/myorange',
+                                               'username' => '@lodev09'
+                                       ],
+                                       [
+                                               'class' => 'banned rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0',
+                                               'image' => 'img/demo/authors/roberto.png',
+                                               'alt' => 'Jovanni Lo',
+                                               'name' => 'Roberto R. (Rails Developer)',
+                                               'work' => 'Partner &amp; Contributor',
+                                               'twitter' => 'https://twitter.com/@sildur',
+                                               'username' => '@sildur',
+                                               'wrap' => 'https://wrapbootstrap.com/user/sildur',
+                                               'title' => 'Contact Roberto'
+                                       ]
+                               ];
+                               foreach ($item as $user) {
+                               ?>
+                               <div class="<?php echo $user['class'];?>">
+                                   <img src="<?php echo $user['image'];?>" alt="<?php echo $user['alt'];?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                                   <div class="ml-2 mr-3">
+                                       <h5 class="m-0">
+                                           <?php echo $user['name'];?>
+                                           <small class="m-0 fw-300">
+                                               <?php echo $user['work'];?>
+                                           </small>
+                                       </h5>
+                                       <a href="<?php echo $user['twitter'];?>" class="text-info fs-sm" target="_blank"><?php echo $user['username'];?></a>
+                                       <a href="<?php echo $user['wrap'];?>" class="text-info fs-sm" target="_blank" title="<?php echo $user['title'];?>"><i class="fal fa-envelope"></i></a>
+                                   </div>
+                               </div>
+                               <?php } ?>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        
+
+        <script src="js/vendors.bundle.js"></script>
+        <script src="js/app.bundle.js"></script>
+        <script>
+            // default list filter
+            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+            // custom response message
+            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+        </script>
+    </body>
+</html>
