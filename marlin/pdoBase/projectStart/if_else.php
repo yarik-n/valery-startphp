c290e5a182504fe8d959ab16421e2565e7e5ef4c
yarik-noyt <arikpisarenko72@gmail.com>
2026-03-17 21:05:46 +0300

    Понять как работает if else

---
+++ marlin/pdoBase/projectStart/if_else.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Result</title>
+<meta charset="utf-8" />
+</head>
+<body
+<?php $a = -3
+?>
+
+<?php if ($a > 0) {?>
+<h2>Переменная а больше нуля</h2>
+<?php } elseif ($a < 0) { ?>
+<h2>Переменная а меньше нуля</h2>
+<?php } else { ?>
+<h2>Переменная равна нулю</h2>
+<?php } ?>
+</body>
+</html>
