d8d2b64ab42362d987a76e1a4d76e3e96d9543ed
yarik-noyt <arikpisarenko72@gmail.com>
2026-03-18 20:34:17 +0300

    Сделать 10 задание

--- marlin/20tasks/for_task_10.php
+++ marlin/20tasks/for_task_10.php
@@ -1,7 +1,6 @@
 <?php
-
 $pdo = new PDO('mysql:host=MySQL-8.4;dbname=tasks', "root", '');
-$sql = 'INSERT INTO task_10 (name) VALUES :name';
+$sql = 'INSERT INTO `task_10`(`name`) VALUES (:username)';
 $statement = $pdo->prepare($sql);
 $statement->execute($_POST);
-
+header('Location:/marlin/20tasks/task_10.php');
